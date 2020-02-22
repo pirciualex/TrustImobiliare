61635e43adad55dc4d187eddcdddbef275d43606
Alex Pirciu <alex.pirciu@zohomail.eu>
2020-02-22 22:08:55 +0200

    Add DbContext class

---
+++ Persistance/TrustImobiliareDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrustImobiliare.Domain.Entities;
+
+namespace Persistance
+{
+    public class TrustImobiliareDbContext : DbContext
+    {
+        public TrustImobiliareDbContext(DbContextOptions<TrustImobiliareDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Household> Households { get; set; }
+        public DbSet<Field> Fields { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Type> Types { get; set; }
+    }
+}
