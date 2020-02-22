7c6f8ed93298c34312efea70566a5e8de12231cc
Alex Pirciu <alex.pirciu@zohomail.eu>
2020-02-23 00:02:28 +0200

    Add configuration for entity framework

--- src/TrustImobiliare.WebUI/Startup.cs
+++ src/TrustImobiliare.WebUI/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Persistance;
 
 namespace TrustImobiliare.WebUI
 {
@@ -26,6 +21,9 @@ namespace TrustImobiliare.WebUI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<TrustImobiliareDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
