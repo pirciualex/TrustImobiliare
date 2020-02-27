d8b4fa669d74587fff72bf1348f7f978314ada17
Alex Pirciu <alex.pirciu@zohomail.eu>
2020-02-28 01:53:11 +0200

    Update Feature and Type entities

--- src/TrustImobiliare.Domain/Entities/Feature.cs
+++ src/TrustImobiliare.Domain/Entities/Feature.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Value { get; set; }
-        public int PropertyId { get; set; }
+        public Property Property { get; set; }
     }
 }
