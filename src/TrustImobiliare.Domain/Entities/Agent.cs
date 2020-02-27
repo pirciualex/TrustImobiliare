51b99af847198250a36c9fe5e026a2a756e9accd
Alex Pirciu <alex.pirciu@zohomail.eu>
2020-02-28 01:06:30 +0200

    Add Agent entity

---
+++ src/TrustImobiliare.Domain/Entities/Agent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TrustImobiliare.Domain.Entities
+{
+    public class Agent
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhotoLink { get; set; }
+        public List<Property> Properties { get; set; }
+    }
+}
