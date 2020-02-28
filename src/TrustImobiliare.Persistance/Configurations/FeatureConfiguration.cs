b3d0474445225e0906c015edc0bd9d7e85efad1a
Alex Pirciu <alex.pirciu@zohomail.eu>
2020-02-28 03:48:50 +0200

    Add configurations for Feature

---
+++ src/TrustImobiliare.Persistance/Configurations/FeatureConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TrustImobiliare.Domain.Entities;
+
+namespace TrustImobiliare.Persistance.Configurations
+{
+    public class FeatureConfiguration:IEntityTypeConfiguration<Feature>
+    {
+        public void Configure(EntityTypeBuilder<Feature> builder)
+        {
+            builder.HasOne(d => d.Property)
+                .WithMany(p => p.Features)
+                .HasForeignKey(d => d.PropertyId)
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasConstraintName("FK_Features_Properties");
+        }
+    }
+}
