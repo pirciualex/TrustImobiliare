8b3b74a812101a60e53d87a9940f49703967c075
Alex Pirciu <alex.pirciu@zohomail.eu>
2020-03-22 00:40:19 +0200

    Add dependency injection specific section to persistance project

---
+++ src/TrustImobiliare.Persistance/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TrustImobiliare.Persistance
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistance(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<TrustImobiliareDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
--- src/TrustImobiliare.WebUI/Startup.cs
+++ src/TrustImobiliare.WebUI/Startup.cs
@@ -20,10 +20,9 @@ namespace TrustImobiliare.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddPersistance(Configuration);
 
-            services.AddDbContext<TrustImobiliareDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
