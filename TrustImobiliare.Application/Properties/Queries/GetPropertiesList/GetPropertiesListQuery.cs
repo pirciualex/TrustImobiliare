5e2f0392d6f6ea61ff24c1ae7a2f9c1694a3e764
Alex Pirciu <alex.pirciu@zohomail.eu>
2020-03-22 03:12:49 +0200

    Add Properties controller with get method for a list of properties

--- TrustImobiliare.Application/Properties/Queries/GetPropertiesList/GetPropertiesListQuery.cs
+++ TrustImobiliare.Application/Properties/Queries/GetPropertiesList/GetPropertiesListQuery.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections.Generic;
-using MediatR;
+﻿using MediatR;
+using System.Collections.Generic;
 using TrustImobiliare.Application.Properties.Dtos;
 
 namespace TrustImobiliare.Application.Properties.Queries.GetPropertiesList
 {
-    public class GetPropertiesListQuery:IRequest<List<PropertyListDto>>
+    public class GetPropertiesListQuery : IRequest<List<PropertyListDto>>
     {
-        
+
     }
 }
---
+++ src/TrustImobiliare.WebUI/Controllers/PropertiesController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TrustImobiliare.Application.Properties.Dtos;
+using TrustImobiliare.Application.Properties.Queries.GetPropertiesList;
+
+namespace TrustImobiliare.WebUI.Controllers
+{
+    [Route("properties")]
+    public class PropertiesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PropertiesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PropertyListDto>>> GetPropertiesAsync()
+        {
+            var query = new GetPropertiesListQuery();
+            var response = await _mediator.Send(query);
+            return Ok(response);
+
+        }
+    }
+}
