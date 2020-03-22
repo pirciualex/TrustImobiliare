e28dc58d3df0f65d72f5d1fe4a93376c481cc45a
Alex Pirciu <alex.pirciu@zohomail.eu>
2020-03-22 02:52:19 +0200

    Add Properties list query and DTO

---
+++ TrustImobiliare.Application/Properties/Dtos/PropertyListDto.cs
@@ -0,0 +1,15 @@
+﻿namespace TrustImobiliare.Application.Properties.Dtos
+{
+    public class PropertyListDto
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public decimal Price { get; set; }
+        public int Surface { get; set; }
+        public string PrimaryPhotoLink { get; set; }
+        public int? Rooms { get; set; }
+        public int? Bathrooms { get; set; }
+        public string Type { get; set; }
+        public string AgentName { get; set; }
+    }
+}
---
+++ TrustImobiliare.Application/Properties/Queries/GetPropertiesList/GetPropertiesListQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using TrustImobiliare.Application.Properties.Dtos;
+
+namespace TrustImobiliare.Application.Properties.Queries.GetPropertiesList
+{
+    public class GetPropertiesListQuery:IRequest<List<PropertyListDto>>
+    {
+        
+    }
+}
